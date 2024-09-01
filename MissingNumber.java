234ed31960c24d182880d231b85e18a1a979e987
XXXalinooi <obiadnastole@gmail.com>
2024-09-01 17:19:22 +0200

    26.08-1.09

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,18 +1,15 @@
-import java.util.Arrays;
-
 public class MissingNumber {
-    public static int findMissNmbMySillySolution(int [] arr){
-        Arrays.sort(arr);
-        for(int i = 0; i < arr.length-1; i++){
-            if(arr[i+1] - arr[i] != 1){
-                return arr[i]+1;
-            }
+    public static int findMissingNumber(int [] arr){
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum = sum + arr[i];
         }
-        return -1;
+        int sumWithoutMissingNumber = (2+arr.length)* (arr.length+1)/2;
+        return sumWithoutMissingNumber - sum;
     }
 
     public static void main(String[] args) {
-        int [] arr = {5, 4, 2, 1, 7, 6};
-        System.out.println(findMissNmbMySillySolution(arr));
+        int [] arr = {1, 2, 5, 7, 8, 3,6};
+        System.out.println(findMissingNumber(arr));
     }
 }
